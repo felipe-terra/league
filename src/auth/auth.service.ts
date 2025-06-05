36c88c3e451ed65cccbeca3c1f2bca6cd825e1f9
Felipe Terra <felipeterra08@gmail.com>
2025-06-05 14:36:26 -0300

    Removido as logs na autenticacao

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,16 +14,7 @@ export class AuthService {
     async login(loginDTO: LoginDTO) {
         const user = await this.usersService.findByEmail(loginDTO.email);
         if (!user) throw new UnauthorizedException('Usuário não encontrado');
-        console.log('Senha fornecida:', loginDTO.password);
-        console.log('Senha armazenada:', user.password);
-        
-        // Tente ambos os métodos para comparar
-        const isPasswordValid1 = await bcrypt.compare(loginDTO.password, user.password);
-        const isPasswordValid2 = bcrypt.compareSync(loginDTO.password, user.password);
-        
-        console.log('Resultado bcrypt.compare:', isPasswordValid1);
-        console.log('Resultado bcrypt.compareSync:', isPasswordValid2);
-        if (!isPasswordValid1) throw new UnauthorizedException('Senha inválida');
+        if (!await bcrypt.compare(loginDTO.password, user.password)) throw new UnauthorizedException('Senha inválida');
         const payload = { id:  user.id, email: user.email, name: user.name};
         const jwt = await this.jwtService.signAsync(payload, {
             expiresIn: '1d',
